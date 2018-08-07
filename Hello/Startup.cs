01158572ddc943cde2fbac76deb087df5cfcaaed
kumar.sendhi <kumar.sendhi@gmail.com>
2018-08-07 19:27:55 +0000

    fix

--- Hello/Startup.cs
+++ Hello/Startup.cs
@@ -27,7 +27,7 @@ namespace Hello
 
             app.Run(async (context) =>
             {
-                await context.Response.WriteAsync("Hello World!");
+                await context.Response.WriteAsync("Hello World! trying CI/CD");
             });
         }
     }
